eddf3797c9965997fa5d2406ef82afd833ec8fe7
Achal <achal.ch@gmail.com>
2022-09-02 14:31:37 +0530

    Netflix code

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -76,6 +76,52 @@ var Netflixdata = [
   ["Documentaries", "Spirituality documentaries", "2760"],
   ["Documentaries", "Sports documentaries", "180"],
   ["Documentaries", "Travel & adventure documentaries", "1159"],
+  ["Dramas", "Army dramas", "11"],
+  ["Dramas", "Biographical dramas", "3179"],
+  ["Dramas", "Crime dramas", "6889"],
+  ["Dramas", "Dramas based on books", "4961"],
+  ["Dramas", "Dramas based on real life", "3653"],
+  ["Dramas", "LGBT Dramas", "500"],
+  ["Dramas", "Political Dramas", "6616"],
+  ["Dramas", "Romantical dramas", "1255"],
+  ["Dramas", "Showbiz dramas", "5012"],
+  ["Dramas", "Social issues dramas", "3947"],
+  ["Dramas", "Sports dramasdsdd", "7243"],
+  ["Foreign movies", "African Movies", "3761"]
+  ["Foreign movies", "Asian action movies", "77323"]
+  ["Foreign movies", "Australian movies", "5230"]
+  ["Foreign movies", "Belgian movies", "262"]
+  ["Foreign movies", "British movies", "10757"]
+  ["Foreign movies", "Chinese movies", "3960"]
+  ["Foreign movies", "Classic Foreign Movies", "32473"]
+  ["Foreign movies", "Dutch movies", "10606"]
+  ["Foreign movies", "Eastern European movies", "5254"]
+  ["Foreign movies", "Foreign Sci-Fi & fantasy", "6485"]
+  ["Foreign movies", "Foreign action & adventure", "11828"]
+  ["Foreign movies", "Foreign comedies", "4426"]
+  ["Foreign movies", "Foreign documentaries", "5161"]
+  ["Foreign movies", "Foreign dramas", "2150"]
+  ["Foreign movies", "Foreign gay & lesbian movies", "8243"]
+  ["Foreign movies", "Foreign horror movies", "8654"]
+  ["Foreign movies", "Foreign thrillers", "10306"]
+  ["Foreign movies", "French movies", "58807"]
+  ["Foreign movies", "German movies", "58886"]
+  ["Foreign movies", "Greek movies", "61115"]
+  ["Foreign movies", "Indian movies", "10463"]
+  ["Foreign movies", "Irish movies", "58750"]
+  ["Foreign movies", "Italian Movies", "8221"]
+  ["Foreign movies", "Japanese movies", "10398"]
+  ["Foreign movies", "Korean movies", "5685"]
+  ["Foreign movies", "Latin american movies", "1613"]
+  ["Foreign movies", "Middle Eastern movies", "5875"]
+  ["Foreign movies", "New Zealand movies", "63782"]
+  ["Foreign movies", "Romantic foreign movies", "7153"]
+  ["Foreign movies", "Russian movies", "11567"]
+  ["Foreign movies", "Scandinavian movies", "9292"]
+  ["Foreign movies", "Southeast Asian Movies", "9196"]
+  ["Foreign movies", "Spanish movies", "58741"]
+  ["Foreign movies", "Taiwanese movies", "434295"]
+  ["Foreign movies", "Turkish movies", "1133133"]
 ];
 
 function makeDropDown(data, level1Filter) {
