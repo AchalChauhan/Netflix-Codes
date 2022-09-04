861209f14e64bcdcd28c4316e407ee2b54ae6487
Achal <achal.ch@gmail.com>
2022-09-04 13:46:02 +0530

    Array Update

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -87,41 +87,152 @@ var Netflixdata = [
   ["Dramas", "Showbiz dramas", "5012"],
   ["Dramas", "Social issues dramas", "3947"],
   ["Dramas", "Sports dramasdsdd", "7243"],
-  ["Foreign movies", "African Movies", "3761"]
-  ["Foreign movies", "Asian action movies", "77323"]
-  ["Foreign movies", "Australian movies", "5230"]
-  ["Foreign movies", "Belgian movies", "262"]
-  ["Foreign movies", "British movies", "10757"]
-  ["Foreign movies", "Chinese movies", "3960"]
-  ["Foreign movies", "Classic Foreign Movies", "32473"]
-  ["Foreign movies", "Dutch movies", "10606"]
-  ["Foreign movies", "Eastern European movies", "5254"]
-  ["Foreign movies", "Foreign Sci-Fi & fantasy", "6485"]
-  ["Foreign movies", "Foreign action & adventure", "11828"]
-  ["Foreign movies", "Foreign comedies", "4426"]
-  ["Foreign movies", "Foreign documentaries", "5161"]
-  ["Foreign movies", "Foreign dramas", "2150"]
-  ["Foreign movies", "Foreign gay & lesbian movies", "8243"]
-  ["Foreign movies", "Foreign horror movies", "8654"]
-  ["Foreign movies", "Foreign thrillers", "10306"]
-  ["Foreign movies", "French movies", "58807"]
-  ["Foreign movies", "German movies", "58886"]
-  ["Foreign movies", "Greek movies", "61115"]
-  ["Foreign movies", "Indian movies", "10463"]
-  ["Foreign movies", "Irish movies", "58750"]
-  ["Foreign movies", "Italian Movies", "8221"]
-  ["Foreign movies", "Japanese movies", "10398"]
-  ["Foreign movies", "Korean movies", "5685"]
-  ["Foreign movies", "Latin american movies", "1613"]
-  ["Foreign movies", "Middle Eastern movies", "5875"]
-  ["Foreign movies", "New Zealand movies", "63782"]
-  ["Foreign movies", "Romantic foreign movies", "7153"]
-  ["Foreign movies", "Russian movies", "11567"]
-  ["Foreign movies", "Scandinavian movies", "9292"]
-  ["Foreign movies", "Southeast Asian Movies", "9196"]
-  ["Foreign movies", "Spanish movies", "58741"]
-  ["Foreign movies", "Taiwanese movies", "434295"]
-  ["Foreign movies", "Turkish movies", "1133133"]
+  ["Foreign movies", "African Movies", "3761"],
+  ["Foreign movies", "Asian action movies", "77323"],
+  ["Foreign movies", "Australian movies", "5230"],
+  ["Foreign movies", "Belgian movies", "262"],
+  ["Foreign movies", "British movies", "10757"],
+  ["Foreign movies", "Chinese movies", "3960"],
+  ["Foreign movies", "Classic Foreign Movies", "32473"],
+  ["Foreign movies", "Dutch movies", "10606"],
+  ["Foreign movies", "Eastern European movies", "5254"],
+  ["Foreign movies", "Foreign Sci-Fi & fantasy", "6485"],
+  ["Foreign movies", "Foreign action & adventure", "11828"],
+  ["Foreign movies", "Foreign comedies", "4426"],
+  ["Foreign movies", "Foreign documentaries", "5161"],
+  ["Foreign movies", "Foreign dramas", "2150"],
+  ["Foreign movies", "Foreign gay & lesbian movies", "8243"],
+  ["Foreign movies", "Foreign horror movies", "8654"],
+  ["Foreign movies", "Foreign thrillers", "10306"],
+  ["Foreign movies", "French movies", "58807"],
+  ["Foreign movies", "German movies", "58886"],
+  ["Foreign movies", "Greek movies", "61115"],
+  ["Foreign movies", "Indian movies", "10463"],
+  ["Foreign movies", "Irish movies", "58750"],
+  ["Foreign movies", "Italian Movies", "8221"],
+  ["Foreign movies", "Japanese movies", "10398"],
+  ["Foreign movies", "Korean movies", "5685"],
+  ["Foreign movies", "Latin american movies", "1613"],
+  ["Foreign movies", "Middle Eastern movies", "5875"],
+  ["Foreign movies", "New Zealand movies", "63782"],
+  ["Foreign movies", "Romantic foreign movies", "7153"],
+  ["Foreign movies", "Russian movies", "11567"],
+  ["Foreign movies", "Scandinavian movies", "9292"],
+  ["Foreign movies", "Southeast Asian Movies", "9196"],
+  ["Foreign movies", "Spanish movies", "58741"],
+  ["Foreign movies", "Taiwanese movies", "434295"],
+  ["Foreign movies", "Turkish movies", "1133133"],
+  ["Horror movies", "Cult horror movies", "10944"],
+  ["Horror movies", "Deep sea horror movies", "45028"],
+  ["Horror movies", "Foreign horror movies", "8654"],
+  ["Horror movies", "Horror comedy", "89585"],
+  ["Horror movies", "Monster movies", "947"],
+  ["Horror movies", "Satanic stories", "6998"],
+  ["Horror movies", "Slasher and serial killer movies", "8646"],
+  ["Horror movies", "Supernatural horror movies", "42023"],
+  ["Horror movies", "Teen screams", "52147"],
+  ["Horror movies", "Vampire horror movies", "75804"],
+  ["Horror movies", "Werewolf horror movies", "75930"],
+  ["Horror movies", "Zombie horror movies", "75405"],
+  ["Independent movies", "Experimental movies", "11079"],
+  ["Independent movies", "Independent action & adventure", "11804"],
+  ["Independent movies", "Independent comedies", "4195"],
+  ["Independent movies", "Independent dramas", "384"],
+  ["Independent movies", "Independent thrillers", "3269"],
+  ["Independent movies", "Romantic independent movies", "9916"],
+  ["LGBTQ", "Gay & Lesbian Comedies", "7120"],
+  ["LGBTQ", "Gay & Lesbian Documentaries", "4720"],
+  ["LGBTQ", "Gay & Lesbian Dramas", "500"],
+  ["LGBTQ", "Gay & Lesbian TV Shows", "65263"],
+  ["LGBTQ", "Romantic Gay & Lesbian Movies", "3329"],
+  ["Music", "Classic musicals", "32392"],
+  ["Music", "Country & western/folk", "1105"],
+  ["Music", "Disney musicals", "59433"],
+  ["Music", "Jazz & easy listening", "10271"],
+  ["Music", "Kids music", "52843"],
+  ["Music", "Latin music", "10741"],
+  ["Music", "Musicals", "13335"],
+  ["Music", "Rock & pop concerts", "3278"],
+  ["Music", "Showbiz musicals", "13573"],
+  ["Music", "Stage musicals", "55774"],
+  ["Music", "Urban & dance concerts", "9472"],
+  ["Music", "World music concerts", "2856"],
+  ["Others", "All movies", "34399"],
+  ["Others", "Art House Movies", "29764"],
+  ["Others", "Epics", "52858"],
+  ["Others", "Experimental movies", "11079"],
+  ["Others", "Faith & spirituality movies", "52804"],
+  ["Others", "Fantasy movies", "9744"],
+  ["Others", "Film noir", "7687"],
+  ["Others", "Gangster movies", "31851"],
+  ["Others", "Only on Netflix", "839338"],
+  ["Others", "Satanic stories", "6998"],
+  ["Others", "Satires", "4922"],
+  ["Others", "Stand-up comedies", "11559"],
+  ["Romantic Movies", "Classic romantic movies", "31273"],
+  ["Romantic Movies", "Quirky romance", "36103"],
+  ["Romantic Movies", "Romantic Comedies", "5475"],
+  ["Romantic Movies", "Romantic dramas", "1255"],
+  ["Romantic Movies", "Romantic favorites", "502675"],
+  ["Romantic Movies", "Romantic foreign movies", "7153"],
+  ["Romantic Movies", "Romantic independent movies", "9916"],
+  ["Romantic Movies", "Steamy romantic movies", "35800"],
+  ["Sci-Fi & Fantasy", "Action Sci - Fi & fantasy", "1568"],
+  ["Sci-Fi & Fantasy", "Alien Sci - Fi", "3327"],
+  ["Sci-Fi & Fantasy", "Classic Sci - Fi & fantasy", "47147"],
+  ["Sci-Fi & Fantasy", "Cult Sci - Fi & fantasy", "4734"],
+  ["Sci-Fi & Fantasy", "Fantasy movies", "9744"],
+  ["Sci-Fi & Fantasy", "Foreign Sci - Fi & fantasy", "6485"],
+  ["Sci-Fi & Fantasy", "Sci - Fi adventure", "6926"],
+  ["Sci-Fi & Fantasy", "Sci - Fi dramas", "3916"],
+  ["Sci-Fi & Fantasy", "Sci - Fi horror movies", "1694"],
+  ["Sci-Fi & Fantasy", "Sci - Fi thrillers", "11014"],
+  ["Sports movies", "Baseball movies", "12339"],
+  ["Sports movies", "Basketball movies", "12762"],
+  ["Sports movies", "Boxing movies", "12443"],
+  ["Sports movies", "Football movies", "12803"],
+  ["Sports movies", "Martial arts movies", "8985"],
+  ["Sports movies", "Martial arts, boxing & wrestling", "6695"],
+  ["Sports movies", "Soccer movies", "12549"],
+  ["Sports movies", "Sports & Fitness", "9327"],
+  ["Sports movies", "Sports comedies", "5286"],
+  ["Sports movies", "Sports documentaries", "180"],
+  ["Sports movies", "Sports dramas", "7243"],
+  ["TV Show", "British TV Shows", "52117"],
+  ["TV Show", "Classic TV shows", "46553"],
+  ["TV Show", "Crime TV shows", "26146"],
+  ["TV Show", "Cult TV shows", "74652"],
+  ["TV Show", "Food & travel", "72436"],
+  ["TV Show", "Kids' TV", "27346"],
+  ["TV Show", "Korean TV shows", "67879"],
+  ["TV Show", "Military TV shows", "25804"],
+  ["TV Show", "Miniseries", "4814"],
+  ["TV Show", "Reality TV", "9833"],
+  ["TV Show", "Science & nature", "52780"],
+  ["TV Show", "TV Sci-Fi & fantasy", "1372"],
+  ["TV Show", "TV action & adventure", "10673"],
+  ["TV Show", "TV comedies", "10375"],
+  ["TV Show", "TV documentaries", "10105"],
+  ["TV Show", "TV dramas", "11714"],
+  ["TV Show", "TV horror", "83059"],
+  ["TV Show", "TV mysteries", "4366"],
+  ["Teen TV shows", "Teen comedies", "3519"],
+  ["Teen TV shows", "Teen dramas", "9299"],
+  ["Teen TV shows", "Teen screams", "52147"],
+  ["Thrillers", "Action thrillers", "43048"],
+  ["Thrillers", "Classic thrillers", "46588"],
+  ["Thrillers", "Crime thrillers", "10499"],
+  ["Thrillers", "Foreign thrillers", "10306"],
+  ["Thrillers", "Gangster movies", "31851"],
+  ["Thrillers", "Independent thrillers", "3269"],
+  ["Thrillers", "Mysteries", "9994"],
+  ["Thrillers", "Political thrillers", "10504"],
+  ["Thrillers", "Psychological thrillers", "5505"],
+  ["Thrillers", "Sci-Fi thrillers", "11014"],
+  ["Thrillers", "Spy thrillers", "9147"],
+  ["Thrillers", "Steamy thrillers", "972"],
+  ["Thrillers", "Supernatural thrillers", "11140"],
+  ["Thrillers", "", ""],
 ];
 
 function makeDropDown(data, level1Filter) {
